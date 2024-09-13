d977bb9fbdb7b26571cc641768bad8a68d2cbd5a
matdomis <matheus.domis2000@gmail.com>
2024-09-12 21:37:04 -0300

    added Erros

---
+++ src/erros.js
@@ -0,0 +1,5 @@
+export const mapaDeErros = {
+	animalInvalido: { erro: "Animal inválido" },
+	quantidadeInvalida: { erro: "Quantidade inválida" },
+	semRecinto: { erro: "Não há recinto viável" }
+};
