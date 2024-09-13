f8648b3cb2d47f82c72a9d8518fd7bdd63db1436
matdomis <matheus.domis2000@gmail.com>
2024-09-12 21:37:34 -0300

    added Recintos Iniciais

---
+++ src/recintos-iniciais.js
@@ -0,0 +1,27 @@
+export const recintosIniciais = [
+	{
+		numero: 1,
+		bioma: ["savana"],
+		tamanho: 10,
+		animais: [{ especie: "MACACO", quantidade: 3 }],
+	},
+	{
+		numero: 2,
+		bioma: ["floresta"],
+		tamanho: 5,
+		animais: [],
+	},
+	{
+		numero: 3,
+		bioma: ["savana", "rio"],
+		tamanho: 7,
+		animais: [{ especie: "GAZELA", quantidade: 1 }],
+	},
+	{ numero: 4, bioma: ["rio"], tamanho: 8, animais: [] },
+	{
+		numero: 5,
+		bioma: ["savana"],
+		tamanho: 9,
+		animais: [{ especie: "LEAO", quantidade: 1 }],
+	},
+];
